699aed32b9eb8f94f2eef5b415d46ff2382207bf
Benjamin <benjminrichie@gmail.com>
2024-04-21 14:06:50 -0700

    7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,7 +12,8 @@ app = Flask(__name__)
 def display_states():
     """Render state_list html page to display States created"""
     states = storage.all()
-    return render_template('7-states_list.html', states=states)
+    sorted_states = sorted(states.values(), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=sorted_states)
 
 
 @app.teardown_appcontext
